eeaccdb887b9ab7a14ff2176c0479de19c295544
whwjdgks123 <76797223+whwjdgks123@users.noreply.github.com>
2021-08-04 20:02:33 +0900

    두번쨰미션
    열공열공

--- scirpt.js
+++ scirpt.js
@@ -1,1 +1,13 @@
-alert("hi");
+//alert("hi");
+
+const a = 5;
+const b = 2;
+
+//const => 자바스크립트의 선언
+
+const myName = "keath";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello" + myName);
