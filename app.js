8d04f79ea94ec0af9d16340838b465cd538a5cfa
whwjdgks123 <76797223+whwjdgks123@users.noreply.github.com>
2021-08-06 21:21:40 +0900

    네번째 미션
    클리어!

--- app.js
+++ app.js
@@ -1,6 +1,64 @@
 //Boolean
-const amIFat = false;
+//const amIFat = false;
 
-let someting;
+//let someting;
 
-console.log(someting, amIFat);
+//console.log(someting, amIFat);
+
+
+//************************************* */
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sund";
+
+const datsOfWeek = ["mon" , "tue" , "wed" , "thu" , "fri" , "sat"];
+
+//const nonsens = [1, 2, "hello", false , null, true , undefined , "nico"];
+
+//Get Item from Array
+ //console.log(datsOfWeek);
+
+//dd one more day to the Array
+datsOfWeek.push("sun");
+
+//console.log(datsOfWeek);
+
+const toBuy = ["potato" , "tomato", "pizza"];
+//toBuy.push("kimbab");
+console.log(toBuy[99999999999]);
+
+
+/******************************* */
+/*
+const playerName ="nico";
+const playerPoints= 121212;
+const PlayerHandsome = false;
+const playerFat = "little bit";
+
+player.name
+player.points
+player.handsome  
+
+
+//player[0] == name
+//player[1] == points
+const player = ["nico", 1212, false, "little bit"];
+*/
+
+const player = {
+    name: "nico",
+    points: 10,
+    fat: true,
+};
+/*
+console.log(player);
+console.log(player.name);
+*/
+console.log(player);
+//player.lastName="potato";
+player.points = player.points + 15;
+console.log(player.points);
