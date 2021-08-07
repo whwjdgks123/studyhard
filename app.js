29a72fc4b8de18ea81d228c60b788db09035cfbd
whwjdgks123 <76797223+whwjdgks123@users.noreply.github.com>
2021-08-07 20:32:25 +0900

    다섯번쨰 미션
    클리어!!

--- app.js
+++ app.js
@@ -7,6 +7,7 @@
 
 
 //************************************* */
+
 const mon = "mon";
 const tue = "tue";
 const wed = "wed";
@@ -29,7 +30,7 @@ datsOfWeek.push("sun");
 
 const toBuy = ["potato" , "tomato", "pizza"];
 //toBuy.push("kimbab");
-console.log(toBuy[99999999999]);
+//console.log(toBuy[99999999999]);
 
 
 /******************************* */
@@ -48,17 +49,69 @@ player.handsome
 //player[1] == points
 const player = ["nico", 1212, false, "little bit"];
 */
-
+/*
 const player = {
     name: "nico",
     points: 10,
     fat: true,
 };
+*/
 /*
 console.log(player);
 console.log(player.name);
 */
+/*
 console.log(player);
 //player.lastName="potato";
 player.points = player.points + 15;
 console.log(player.points);
+*/
+
+/************************** */
+/*
+
+console.log("Hello my name is Nico");
+console.log("Hello my name is Dal");
+console.log("Hello my name is Shigatsu");
+console.log("Hello my name is Flynn");
+console.log("Hello my name is Nico");
+console.log("Hello my name is Nico");
+console.log("Hello my name is Nico");
+console.log("Hello my name is Nico");
+console.log("Hello my name is Nico");
+*/
+/*
+function sayHello(nameOfPerson,age) {
+  console.log("Hello my name is " + nameOfPerson + " and I'm "+ age);
+}
+
+sayHello("nico", 10);
+sayHello("dal", 23);
+sayHello("lynn", 21);
+*/
+
+function  plus(firstNumber, secondNumber) {
+    console.log(firstNumber + secondNumber);
+}
+
+function divide(a,b) {
+    console.log(a / b);}
+
+plus(8, 60);
+divide(98,20);
+
+const player ={
+    name:"nico",
+    sayHello: function (otherPersonsName){
+        console.log("hello! " + otherPersonsName + " nice to meet you");
+    },
+};
+
+console.log(player.name);
+player.sayHello("lynn");
+player.sayHello("nico");
+
+function  plus(a, b) {
+    console.log(a + b);
+}
+plus(5,5);
